0df1bccb330415f193148e3ad7733213ece55bce
Farid Seidu <Versedcamel153@gmail.com>
2024-06-28 21:35:15 +0000

    added control-flow directory

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,20 @@
+task = input("Enter your task: ")
+priority = input("Priority (high/medium/low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
+
+message = "that requires immediate attention today!"
+
+match priority:
+    case "high" | _ if time_bound == "yes":
+        print(f"Reminder: '{task}' is a high priority task {message}")
+    case "high":
+        print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
+    case "low" | _ if time_bound == "yes":
+        print(f"Reminder: '{task}' is a low priority task {message}")
+    case "low":
+        print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
+    case "medium" | _ if time_bound == "yes":
+        print(f"Reminder: '{task}' is a medium priority task {message} ")
+    case "medium":
+        print(f"Note:'{task}' is a medium priority task. Consider completing it when you have free time.")
+
---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}")
+    case "/" | _ if num1 == 0 or num2 == 0:    
+        print(f"Cannot divide by zero.")
+    case "/":
+        result = num1 / num2
+        print(f"The result is {result}")
+        
