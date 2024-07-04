d281f58cdbba5b63dcee3ceb94ddb5d2d68e3b57
Farid Seidu <Versedcamel153@gmail.com>
2024-07-04 19:55:35 +0000

    Added fns_and_dsa

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@ def perform_operation(num1, num2, operation):
     if operation=='add':
         result = num1 + num2
     elif operation=='subtract':
-        result = num1 = num2
+        result = num1 - num2
     elif operation =='multiply':
         result = num1 * num2
     elif operation =='divide':
