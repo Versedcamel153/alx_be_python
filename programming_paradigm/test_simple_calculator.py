a4011f05d26199764b1a4b2b58d01feffc062bf6
Farid Seidu <Versedcamel153@gmail.com>
2024-07-14 21:03:43 +0000

    added paradigm directory

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_addion(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(10, 10), 20)
     
     def test_subtraction(self):
